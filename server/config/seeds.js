190bcea597ce1eb0d422562e9401dff4ee55709f
Joseph Bralic <joseph.bralic@gmail.com>
2023-11-21 18:45:22 +1030

    finished art website

---
+++ server/config/seeds.js
@@ -0,0 +1,53 @@
+const db = require('./connection');
+const { User, Product, Category } = require('../models');
+const cleanDB = require('./cleanDB');
+
+db.once('open', async () => {
+  await cleanDB('Category', 'categories');
+  await cleanDB('Product', 'products');
+  await cleanDB('User', 'users');
+
+  const categories = await Category.insertMany([
+    { name: 'canvas' },
+    { name: 'Figure Drawings' },
+    { name: 'Portraits' },
+  ]);
+
+  console.log('categories seeded');
+
+  const products = await Product.insertMany([
+    {
+      name: 'Fairytale',
+      description: 'Full colour digital painting',
+      image: '/Images/p1.jpg',
+      category: categories[0]._id,
+      price: 120,
+      quantity: 10,
+    },
+  ]);
+
+  console.log('products seeded');
+
+  await User.create({
+    firstName: 'Pamela',
+    lastName: 'Washington',
+    email: 'pamela@testmail.com',
+    password: 'password12345',
+    orders: [
+      {
+        products: [products[0]._id, products[0]._id, products[0]._id],
+      },
+    ],
+  });
+
+  await User.create({
+    firstName: 'Elijah',
+    lastName: 'Holt',
+    email: 'eholt@testmail.com',
+    password: 'password12345',
+  });
+
+  console.log('users seeded');
+
+  process.exit();
+});
